5c513741806cb0a5343f9e98b4dd85a2269b4e20
Mikazuuu07 <mikazuuu07@nusantararom.org>
2022-07-08 16:42:37 +0700

    Initial Website Commit
    Signed-off-by: Mikazuuu07 <mikazuuu07@nusantararom.org>

---
+++ jancoksekrip/sekrip.js
@@ -0,0 +1,25 @@
+const kntlElement = ['Freelancer', 'Web Developer', 'Graphic Designer', 'Artist', 'Mechanical Engineering'];
+let count = 0;
+let kntlIndex = 0;
+let currentKntl = '';
+let words = '';
+
+(function ngentot(){
+
+	if(count == kntlElement.length){
+		 count = 0;
+	}
+
+	currentKntl = kntlElement[count];
+
+	words = currentKntl.slice(0, ++kntlIndex);
+	document.querySelector('.ngentot').textContent = words;
+
+	if(words.length == currentKntl.length){
+		count++;
+		kntlIndex = 0;
+	}
+
+	setTimeout(ngentot, 150);
+
+})();
