28483c6182a2e52dd0afc3d74c27d7b389d4fdde
Mikazuuu07 <mikazuuu07@nusantararom.org>
2022-07-28 20:30:28 +0700

    Add animation and redesign the panel
    Signed-off-by: Mikazuuu07 <mikazuuu07@nusantararom.org>

--- jancoksekrip/sekrip.js
+++ jancoksekrip/sekrip.js
@@ -1,3 +1,5 @@
+  AOS.init();
+
 const kntlElement = ['Freelancer', 'Web Developer', 'Graphic Designer', 'Artist', 'Mechanical Engineering'];
 let count = 0;
 let kntlIndex = 0;
@@ -44,3 +46,27 @@ document.addEventListener('click', function(event) {
   if (!Jancok.contains(event.target))
     Cok();
 });
+
+const budi = document.body;
+let sekrolAkhir = 0;
+
+window.addEventListener('scroll', () => {
+	const sekrolSaiki = window.pageYOffset
+
+	if (sekrolSaiki <= 0) {
+		budi.classList.remove("sekrol-munggah")
+	}
+
+	if (sekrolSaiki > sekrolAkhir && !budi.classList.contains("sekrol-mudun")) {
+		budi.classList.remove("sekrol-munggah")
+		budi.classList.add("sekrol-mudun")
+	}
+
+	if (sekrolSaiki < sekrolAkhir && budi.classList.contains("sekrol-mudun")) {
+	budi.classList.remove("sekrol-mudun")
+	budi.classList.add("sekrol-munggah")
+	}
+
+
+	sekrolAkhir = sekrolSaiki;
+})
